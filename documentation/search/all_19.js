c0c6abb00a50a6f7790a432625fea173a20de46f
MFC Action <>
2026-03-02 00:30:01 +0000

    Docs @ 4ee892c

--- documentation/search/all_19.js
+++ documentation/search/all_19.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['qbmm_20moment_20transport_20qbmm_20true_20bryngelson20_0',['6.1.6 QBMM Moment Transport (&lt;span class=&quot;tt&quot;&gt;qbmm = .true.&lt;/span&gt;) (&lt;a class=&quot;el&quot; href=&quot;citelist.html#CITEREF_bryngelson20&quot;&gt;[7]&lt;/a&gt;)',['../equations.html#autotoc_md160',1,'']]],
-  ['qbmm_20true_20bryngelson20_1',['6.1.6 QBMM Moment Transport (&lt;span class=&quot;tt&quot;&gt;qbmm = .true.&lt;/span&gt;) (&lt;a class=&quot;el&quot; href=&quot;citelist.html#CITEREF_bryngelson20&quot;&gt;[7]&lt;/a&gt;)',['../equations.html#autotoc_md160',1,'']]],
+  ['qbmm_20moment_20transport_20qbmm_20true_20bryngelson20_0',['6.1.6 QBMM Moment Transport (&lt;span class=&quot;tt&quot;&gt;qbmm = .true.&lt;/span&gt;) (&lt;a class=&quot;el&quot; href=&quot;citelist.html#CITEREF_bryngelson20&quot;&gt;[7]&lt;/a&gt;)',['../equations.html#autotoc_md161',1,'']]],
+  ['qbmm_20true_20bryngelson20_1',['6.1.6 QBMM Moment Transport (&lt;span class=&quot;tt&quot;&gt;qbmm = .true.&lt;/span&gt;) (&lt;a class=&quot;el&quot; href=&quot;citelist.html#CITEREF_bryngelson20&quot;&gt;[7]&lt;/a&gt;)',['../equations.html#autotoc_md161',1,'']]],
   ['quick_20parameter_20reference_2',['📖 Quick Parameter Reference',['../case_constraints.html#quick-parameter-reference',1,'']]],
-  ['quick_20reference_3',['Quick Reference',['../cli-reference.html#autotoc_md45',1,'Quick Reference'],['../troubleshooting.html#autotoc_md476',1,'Quick Reference']]]
+  ['quick_20reference_3',['Quick Reference',['../cli-reference.html#autotoc_md45',1,'Quick Reference'],['../troubleshooting.html#autotoc_md479',1,'Quick Reference']]],
+  ['quick_20visualization_20with_20mfc_20sh_20viz_4',['Quick visualization with &lt;span class=&quot;tt&quot;&gt;./mfc.sh viz&lt;/span&gt;',['../visualization.html#autotoc_md536',1,'']]]
 ];
--- documentation/search/pages_20.js
+++ documentation/search/pages_20.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['x_5fdomain_0',['x_domain',['../parameters.html#x_domain',1,'']]],
-  ['x_5foutput_1',['x_output',['../parameters.html#x_output',1,'']]]
+  ['x_20not_20found_0',['&quot;Variable &apos;X&apos; not found&quot;',['../troubleshooting.html#autotoc_md529',1,'']]],
+  ['x_5fdomain_1',['x_domain',['../parameters.html#x_domain',1,'']]],
+  ['x_5foutput_2',['x_output',['../parameters.html#x_output',1,'']]]
 ];
